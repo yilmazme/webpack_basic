5a069066c386b7ad80c8a05cd8ad5ff5d8850051
myilmaz <myilmaz@subilgi.com.tr>
2021-08-09 16:21:52 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ console.log(sayHello("Mehmet"));
 
 let array = [1,2,3,4,]
 
+let obj = {a:"alpha", b:"bravo"}
+let newObj = {...obj, c:"charlie"}
 
 
+console.log(newObj)
 console.log(array.includes(3))
